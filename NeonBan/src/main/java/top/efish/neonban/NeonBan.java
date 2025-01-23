a1a408dc4bd0a07129c5ed5749ae40c633cf3fa8
栢兹氧 <baiziyang2022@yeah.net>
2025-01-23 18:49:31 +0800

    Update

---
+++ NeonBan/src/main/java/top/efish/neonban/NeonBan.java
@@ -0,0 +1,17 @@
+package top.efish.neonban;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NeonBan extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
